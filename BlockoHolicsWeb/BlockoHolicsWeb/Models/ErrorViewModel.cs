f796d60d1faf585b66d50f2b0add6257595c64f6
sspeev <peevstoyan05@gmail.com>
2026-03-30 21:44:14 +0300

    Initial website commit

---
+++ BlockoHolicsWeb/BlockoHolicsWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlockoHolicsWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
