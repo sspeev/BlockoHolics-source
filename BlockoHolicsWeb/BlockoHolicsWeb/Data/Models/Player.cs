1ed4f7ffe96d52f4d26c124846f14abca63df797
sspeev <peevstoyan05@gmail.com>
2026-03-31 14:18:35 +0300

    Add EF Core support and Player entity to project
    Introduce Entity Framework Core with SQL Server configuration. Add Player entity and related constants. Register BlockoHolicsDbContext in DI. Clean up .csproj and update appsettings.json with connection string.

---
+++ BlockoHolicsWeb/BlockoHolicsWeb/Data/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static BlockoHolicsWeb.Constants.DataConstants.PlayerConstants;
+
+namespace BlockoHolicsWeb.Data.Models
+{
+    [Keyless]
+    [Comment("Represents a player in the game.")]
+    public class Player
+    {
+        [MaxLength(MaxPlayerName)]
+        public string? Name { get; set; }
+
+        [Required]
+        public DateTime Time { get; set; }
+    }
+}
--- BlockoHolicsWeb/BlockoHolicsWeb/Program.cs
+++ BlockoHolicsWeb/BlockoHolicsWeb/Program.cs
@@ -1,7 +1,14 @@
+using BlockoHolicsWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BlockoHolicsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
